4d919b44a7518c6bcaae2ead542300f1ab8ed55f
Shubhendu Kumar <34783719+shubhendukr@users.noreply.github.com>
2018-03-17 14:46:13 +0800

    Adding detailed documentation for the methods

--- media.py
+++ media.py
@@ -5,13 +5,22 @@ class Movie():
 	""" This class provides a way to store movie related information"""
     def __init__(
       self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """ Inits a Movie object
+        Args:
+        title = a string of the movie's title
+        storyline = a string briefly description of movie
+        poster_url = a string containing a URL to movie's poster image
+        trailer_youtube_url = a string containing a movie's trailer URL
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ Opens trailer in a web browser """
         webbrowser.open(self.trailer_youtube_url)
 
     def show_poster(self):
+        """ Displays poster image of the movie """
         webbrowser.open(self.poster_image_url)
